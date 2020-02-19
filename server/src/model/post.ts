2e0a9bd42752f8b478574a8e738e221efd719cae
withparadox2 <withparadox2@gmail.com>
2020-02-19 13:24:36 +1300

    Typescript requires lowercase as type declaration

--- server/src/model/post.ts
+++ server/src/model/post.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-
+// TODO Tags, Comments, Update time...
 const postSchema = new mongoose.Schema({
     title: String,
     content: String,
@@ -7,8 +7,8 @@ const postSchema = new mongoose.Schema({
 })
 
 interface IPost extends mongoose.Document {
-    title: String,
-    content: String,
+    title: string,
+    content: string,
     createTime: Date
 }
 
